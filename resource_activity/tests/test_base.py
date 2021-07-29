bcf37e1ba2941de68c6c009873f586ae247851f2
robin.keunen <robin@keunen.net>
2021-07-29 19:39:13 +0200

    [REF] resource_activity: split resource_activity_guide

--- resource_activity/tests/test_base.py
+++ resource_activity/tests/test_base.py
@@ -10,18 +10,13 @@ class TestResourceActivityBase(common.TransactionCase):
     def setUp(self):
         super(TestResourceActivityBase, self).setUp()
         self.partner_demo = self.browse_ref("base.partner_demo")
-        self.bike_category = self.browse_ref("resource_planning.resource_category_bike_demo")
-        self.bike_product = self.browse_ref("resource_activity.product_product_bike_rent_demo")
-        self.guide_partner_1 = self.browse_ref(
-            "resource_activity.res_partner_friendly_guide_demo"
+        self.bike_category = self.browse_ref(
+            "resource_planning.resource_category_bike_demo"
         )
-        self.guide_partner_2 = self.browse_ref(
-            "resource_activity.res_partner_mean_guide_demo"
+        self.bike_product = self.browse_ref(
+            "resource_activity.product_product_bike_rent_demo"
         )
         self.main_location = self.browse_ref("resource_planning.main_location")
         self.activity_type = self.browse_ref(
             "resource_activity.resource_activity_type_tour_demo"
-        )
-        self.guide_product = self.browse_ref(
-            "resource_activity.guide_product_product_demo"
         )
