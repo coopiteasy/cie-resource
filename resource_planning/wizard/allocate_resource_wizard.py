def5bac2225fbdb9d814bfcf53062a115b8674e1
robin.keunen <robin@keunen.net>
2021-08-20 11:34:19 +0200

    [MIG] resource_planning: Migration to 11.0

--- resource_planning/wizard/allocate_resource_wizard.py
+++ resource_planning/wizard/allocate_resource_wizard.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# Copyright 2018 Coop IT Easy SCRLfs.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
