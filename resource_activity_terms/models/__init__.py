694c738951694419faddc5d1f619ed10bcb31aea
robin.keunen <robin@keunen.net>
2021-11-18 22:53:00 +0100

    [IMP] resource_activity_terms: translate note content
    Note content is translated when set on sale order. We do this
    because note must be editable after gtranslation.
    
    + refactoring: gather all order related manipulation on sale.order

--- resource_activity_terms/models/__init__.py
+++ resource_activity_terms/models/__init__.py
@@ -1,7 +1,6 @@
 from . import res_company_note
 from . import res_company_terms
 from . import res_config_settings
-from . import resource_activity
 from . import resource_location
 from . import resource_location_terms
 from . import sale_order
--- resource_activity_terms/models/sale_order.py
+++ resource_activity_terms/models/sale_order.py
@@ -7,17 +7,39 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.model
-    def _default_note_html(self):
+    def _default_company_note(self):
         note_id = (
             self.env["ir.config_parameter"]
             .sudo()
             .get_param("resource_activity_terms.default_sale_note_html_id")
         )
-        note = self.env["res.company.note"].browse(int(note_id))
-        return note.content
+        return self.env["res.company.note"].browse(int(note_id))
+
+    @api.model
+    def create(self, vals):
+        order = super(SaleOrder, self).create(vals)
+        # sets note CONTENT for order location and activity type
+        # if none, sets default company note.
+        # The note content is translated
+
+        activity = order.activity_id
+
+        if not activity:
+            return order
+
+        sale_note = activity.location_id.terms_ids.filtered(
+            lambda r: r.note_id.active
+            and r.location_id == activity.location_id
+            and r.activity_type_id == activity.activity_type
+        ).note_id
+
+        if not sale_note:
+            sale_note = self._default_company_note()
+
+        content = sale_note.with_context(lang=order.partner_id.lang).content
+        order.note_html = content
+        return order
 
     note_html = fields.Html(
         string="Rich Terms and conditions",
-        default=lambda self: self._default_note_html(),
     )
