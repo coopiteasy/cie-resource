11b3c7e051f94dde643920851f10765f8c690cbb
hugues de keyzer <odoo@hugues.info>
2021-12-10 17:44:02 +0100

    [REL] resource_activity_delivery 12.0.1.0.1

--- resource_activity_delivery/__manifest__.py
+++ resource_activity_delivery/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Resource Activity Deliveries",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "depends": [
         "resource_activity",
     ],
