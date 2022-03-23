bc9ce7f4efb2cf1670c754de8943632af01b4971
hugues de keyzer <odoo@hugues.info>
2022-03-23 09:14:48 +0100

    [REL] resource_activity_trainer 12.0.1.1.0

--- resource_activity_trainer/__manifest__.py
+++ resource_activity_trainer/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Resource Activity Trainer",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "depends": [
         "resource_activity",
     ],
